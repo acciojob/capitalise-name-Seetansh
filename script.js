ea06e7de617ce35dbf75ea3d7a40543be0e51e5b
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-05-13 18:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+window.onload = function () {
+
+    const input = document.getElementById("fname");
+
+    input.addEventListener('blur', function () {
+        input.value = input.value.toUpperCase();
+    });
+}
